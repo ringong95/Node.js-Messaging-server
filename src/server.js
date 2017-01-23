e568cb473003f6c8a1bf6d6f9d5f73b5eab19c2c
Ringo <ringong95@gmail.com>
2017-01-23 13:56:06 -0800

    Working basic text chat

--- src/server.js
+++ src/server.js
@@ -1,21 +1,27 @@
 const net = require('net')
 const server = net.createServer()
 server.on('connection', handleConnection)
+const connections = []
 server.listen(9000, function () {
   console.log('server listening to %j', server.address())
 })
 
 function handleConnection (socket) {
+
+  connections.push(socket)
+
   const remoteAddress = `${socket.remoteAddress}:${socket.remotePort}`
   console.log(`new client connection from ${remoteAddress}`)
   console.log(socket.data)
   socket.setEncoding('utf8')
 
   socket.on('data', (data) => {
-    socket.write(`Echo, you fuck nuts sent: ${data} `)
+    connections.forEach((connection) => connection.write(` ${remoteAddress} says :${data}`))
+    // socket.write(`${remoteAddress} says: ${data} `)
   })
   socket.on('close', () => {
     console.log(`Closing connection from:${remoteAddress}`)
+    // Todo rmeove connection from array on leave
   })
   socket.on('error', (error) => {
     console.log(`${remoteAddress} error, ${error}`)
